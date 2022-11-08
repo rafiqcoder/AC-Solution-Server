31816d4b82a475ebedd2cc0460f8c066d634b26f
rafiqcoder <rafiqcoder@gmail.com>
2022-11-08 19:41:31 +0600

    connected to mongodb and added first service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient,ServerApiVersion,ObjectId,MongoCursorInUseError } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
@@ -10,6 +11,35 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://AcSolutions:x7qezkoFdQPHxa7y@cluster0.1rvc7ql.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri,{ useNewUrlParser: true,useUnifiedTopology: true,serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const Services = client.db('AcSolutions').collection('Services');
+
+    try {
+        app.post('/add-service',async (req,res) => {
+            const service = req.body;
+            const result = await Services.insertOne(service);
+            console.log(result);
+            res.send({
+                message: 'success',
+                data: result,
+            });
+        })
+
+
+
+    } finally {
+        // await client.close();
+
+    }
+
+
+}
+run().catch(error => console.error(error));
 
 app.listen(port,() => {
     console.log(`Example app listening at http://localhost:${port}`)
