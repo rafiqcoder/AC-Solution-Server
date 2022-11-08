411740abfa9dd9fb20768048a7f36da554ba1f1a
rafiqcoder <rafiqcoder@gmail.com>
2022-11-08 20:54:43 +0600

    added get method to get single service data

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/services/:id',async (req,res) => {
+            const { id } = req.params;
+
+            const query = { _id: ObjectId(id) };
+            const cursor = Services.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+
+        });
+
+
     } finally {
         // await client.close();
 
